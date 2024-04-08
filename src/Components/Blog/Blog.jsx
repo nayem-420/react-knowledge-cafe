4098cdb7c50e24e36c2fc512902bb7bf5721a9d2
nayem181 <nayemimranhasan@gmail.com>
2024-04-09 02:07:16 +0600

    added blog proptypes in jsx

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
+    const {title} = blog;
     return (
         <div>
-            
+            <h2 className="text-4xl">{title}</h2>
         </div>
     );
 };
 
-Blog.propTypes{
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
-}
+};
+
 
 export default Blog;
